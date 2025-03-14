3cd2d43dbafa90ed3c28e8a5359fd53374f5204b
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-03-14 15:51:30 +0100

    add: database and express app

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// sttivazione server
+app.listen(port, () => {
+    console.log(`Server "movies" in funzione sulla porta: ${port}`);
+});
